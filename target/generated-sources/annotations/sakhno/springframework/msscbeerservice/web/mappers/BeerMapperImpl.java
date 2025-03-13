02b6f80665057b8b25bac2e747fbbd6affc12606
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-03-13 15:06:31 +0600

    реализация имитации варки пива

--- target/generated-sources/annotations/sakhno/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/sakhno/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-10T21:18:42+0600",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.1 (Oracle Corporation)"
+    date = "2025-03-13T15:04:46+0600",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Amazon.com Inc.)"
 )
 @Component
 @Primary
