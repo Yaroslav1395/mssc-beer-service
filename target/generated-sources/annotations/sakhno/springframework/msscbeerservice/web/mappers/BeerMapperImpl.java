435a85b5aabf62b1e7a4f8ce4fe539ba594e4b7c
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-03-26 12:23:47 +0600

    подключение server config

---
+++ target/generated-sources/annotations/sakhno/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package sakhno.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-26T11:46:22+0600",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Amazon.com Inc.)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator {
+}
