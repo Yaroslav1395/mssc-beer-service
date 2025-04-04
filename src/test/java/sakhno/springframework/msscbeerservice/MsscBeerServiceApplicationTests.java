700d07d8be2d72aa07780e34ac2bd25c1e3e6189
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-04-04 16:43:39 +0600

    настройка докер компос для склада заглушки

--- src/test/java/sakhno/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/sakhno/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -3,8 +3,10 @@ package sakhno.springframework.msscbeerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:application.properties", "classpath:bootstrap.properties"})
 public class MsscBeerServiceApplicationTests {
 
     @Test
